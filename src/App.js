ea57c4b680ab6d2e5937b1f119908c2047e8c7c6
unknown <janais1991@gmail.com>
2019-01-19 17:00:41 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyAdYXd2A50qFgrA4h-5ps2Dn5C1OAuX2WU",
+  authDomain: "bloc-chat-react-project.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-project.firebaseio.com",
+  projectId: "bloc-chat-react-project",
+  storageBucket: "bloc-chat-react-project.appspot.com",
+  messagingSenderId: "102814688722"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
