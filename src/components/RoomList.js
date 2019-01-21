3d405ffe12a1d023335c949e6351aa4a0a8a60b7
unknown <janais1991@gmail.com>
2019-01-21 17:49:22 -0600

    Working on new room creation form

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class RoomList extends Component {
   constructor(props) {
     super(props);
-    this.state = { rooms: [] };
+    this.state = {
+      rooms: [],
+      value: ''
+    };
 
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
@@ -16,15 +19,29 @@ class RoomList extends Component {
     });
   }
 
+  createRoom(){
+    this.roomsRef.push({
+      name: this.state.value // should be equal to text input value
+    });
+  }
+
   render() {
     return (
       <section>
-      {
-        this.state.rooms.map( (room, index) =>
-        <p className="rooms" key={index} > {room.name}</p>,
-        console.log(this.state.rooms)
-        )
-      }
+        {
+          this.state.rooms.map( (room, index) =>
+          <p className="rooms" key={index} > {room.name}</p>,
+          console.log(this.state.rooms)
+          )
+        }
+
+        <form onSubmit={this.createRoom}>
+          <label>
+            New Room Name:
+            <input type="text" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </section>
     );
   }
