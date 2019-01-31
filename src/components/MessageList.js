bfcd639dd548da3db07aa1fc84ed264d9c3a5ddb
unknown <janais1991@gmail.com>
2019-01-31 15:37:20 -0600

    Added MessageList.js component

---
+++ src/components/MessageList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class MessageList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    this.messagesRef = this.props.firebase.database().ref('messages');
+  }
+
+  componentDidMount(){
+    this.messagesRef.on('child_added', snapshot => {
+      const message = snapshot.val();
+      message.key = snapshot.key;
+      this.setState({ messages: this.state.messages.concat( message ) })
+    });
+  }
+
+  render() {
+    return (
+      <section>
+      </section>
+    );
+  }
+}
+
+export default MessageList;
